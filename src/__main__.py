986f231c123efee4cdb2672f868113515b382ddf
ovega14 <vega00087@gmail.com>
2025-08-29 01:31:09 +0200

    implemented `run_submodule`

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+import runpy
+
+
+def run_submodule(name: str) -> None:
+    """Executes a submodule as a .py file."""
+    runpy.run_module(name, run_name='__main__')
+
+
+run_submodule('linalg')
+run_submodule('gens')
+run_submodule('sun')
+run_submodule('action')
