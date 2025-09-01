83a32338aedef7d3299e0bd6385c8c46f0b13d41
ovega14 <vega00087@gmail.com>
2025-09-02 01:55:16 +0200

    implemented `canonicalize_su3` function

--- src/canon.py
+++ src/canon.py
@@ -17,3 +17,60 @@ def canonicalize_su2(thW):
     thW[..., 0] = wrap(thW[..., 0]).abs()  # map to [0, pi]
     thW[..., 1] = -thW[..., 0]  # second angle is negative first
     return thW
+
+
+def canonicalize_su3(thW):
+    """
+    Canonicalizes a set of SU(3) eigenangles :math:`(\theta_1, theta_2, \theta_3)` by
+
+        1.) Project onto hyperplane defined by :math:`\sum_i \theta_i = 0`,
+        2.) Wrap onto canonical hexagon centered at the identity
+        3.) Map into coordinates :math:`(a, b, c)` from angles :math:`\theta_i`
+        4.) Impose hexagonal constraints by wrapping :math:`a, b, c` into [-0.5, 0.5]
+        5.) Round and shift into the centered hexagon
+
+    Args:
+        thW: Batch of non-canonicalzied SU(3) eigenangles
+
+    Returns:
+        Canonicalized batch of SU(3) eigenangles summing to zero
+    
+    """
+    thW[..., -1] -= torch.sum(thW, dim=-1)  # sum_i theta_i = 0
+    v = thW.reshape(-1, 3)
+
+    U = 2*np.pi * torch.tensor([  # map (a,b,c) -> v = (th1,th2,th3)
+        [1, 0, -1],
+        [0, -1, 1],
+        [-1, 1, 0]
+    ])
+    U_inv = torch.tensor([  # map v -> (a,b,c)
+        [1, 0, -1],
+        [0, -1, 1],
+        [-1, 1, 0]
+    ]) / (6*np.pi)
+
+    kappa = U_inv @ torch.transpose(v, 0, 1)
+    a, b, c, = kappa[0], kappa[1], kappa[2]
+
+    k = (b + c) / 2
+    a -= k
+    b -= k
+    c -= k
+    a -= torch.round(a)
+
+    k = torch.round(b)
+    b -= k
+    c += k
+    b -= torch.round(b - (a + c)/2))
+
+    k = (b + c) / 2
+    a -= k
+    b -= k
+    c -= k
+    a -= torch.round(a)
+    c -= torch.round(c - (a + b)/2))
+
+    kappa = torch.stack([a, b, c], dim=0)
+
+    return torch.transpose(U @ kappa, 0, 1).reshape(thW.shape)
