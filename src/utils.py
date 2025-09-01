be2e94f96ce25b0ba2a884c32198a682dd903bcd
ovega14 <vega00087@gmail.com>
2025-09-01 21:48:53 +0200

    implemented `wrap` function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 """Miscellaneous utilities."""
+import torch
+import numpy as np
+
 from torch import Tensor
 from numpy.typing import NDArray
 from typing import Any
@@ -19,3 +22,31 @@ def grab(x: Tensor | Any) -> NDArray | Any:
         return x.detach().cpu().numpy()
     else:
         return x
+
+
+def wrap(theta: Tensor | NDArray) -> Tensor | NDArray:
+    """
+    Wraps a non-compact input variable into the
+    compact interval :math:`[-\pi, \pi]`.
+
+    Args:
+        theta (Tensor, NDArray): Non-compact, real-valued input
+
+    Returns:
+        Input wrapped around [-pi, pi]
+    """    
+    return (theta + np.pi) % (2*np.pi) - np.pi
+
+
+def _test_wrap():
+    print('[Testing wrap...]')
+    batch_size = 5
+    x = 20 * torch.randn((batch_size))
+    wx = wrap(x)
+    pi = np.pi * torch.ones_like(x)
+    assert torch.all((-pi < wx) & (wx < pi)), \
+        '[FAILED: Output must be within (-pi, pi)]'
+    print('[PASSED]')
+
+
+if __name__ == '__main__': _test_wrap()
