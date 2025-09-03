89593fb66c05a10aa164cb1cf4b98c84e5a89cf6
ovega14 <vega00087@gmail.com>
2025-09-03 06:19:20 +0200

    updated `run_submodule` function

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,9 @@ import runpy
 
 def run_submodule(name: str) -> None:
     """Executes a submodule as a .py file."""
+    print(f'Running submodule {name}...')
     runpy.run_module(name, run_name='__main__')
+    print()
 
 
 run_submodule('linalg')
