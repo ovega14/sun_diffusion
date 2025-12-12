60080545aa0843b3589545ca2a70168d61cba035
ovega14 <vega00087@gmail.com>
2025-12-12 13:34:31 -0600

    implemented `Action` abc and fixed module formatting

--- src/action.py
+++ src/action.py
@@ -1,5 +1,7 @@
 """Physical actions as functions of field configurations."""
 import torch
+from abc import ABC, abstractmethod
+from typing import Optional
 
 from .linalg import trace, adjoint
 from .sun import random_sun_element, random_sun_lattice, mat_angle
@@ -12,10 +14,21 @@ __all__ = [
 ]
 
 
-class SUNToyAction:
-    """
-    Toy matrix action defined in terms of a single
-    SU(N) degree of freedom on a single lattice site.
+class Action(ABC):
+    """Abstract base class for all physics actions."""
+    @abstractmethod
+    def __call__(self, cfgs):
+        """Evaluates the action on a batch of configurations `cfgs`."""
+        raise NotImplementedError()
+
+
+class SUNToyAction(Action):
+    r"""
+    Toy action on a single :math:`{\rm SU}(N)` matrix defined by
+    
+    .. math::
+
+        S(U) = -\frac{\beta}{N}{\rm Re}{\rm Tr}(U)
 
     Args:
         beta (float): Coupling strength
@@ -27,14 +40,17 @@ class SUNToyAction:
     def beta(self) -> float:
         return self.__beta
 
-    def __call__(self, U):
-        assert len(U.shape) == 3, \
-            'U must have shape [B, Nc, Nc]'
+    def __call__(self, U: torch.Tensor) -> torch.Tensor:
+        """Computes the action as a function of group elements `U`."""
+        assert len(U.shape) == 3, 'U must have shape [B, Nc, Nc]'
         Nc = U.shape[-1]
-        return -self.beta * trace(U).real / Nc
+        return -(self.beta / Nc) * trace(U).real
 
 
-def apply_gauge_transform(U, V=None):
+def apply_gauge_transform(
+    U: torch.Tensor, 
+    V: Optional[torch.Tensor] = None
+) -> torch.Tensor:
     """Applies a unitary conjugation to an input element `U`."""
     if V is None:
         V = random_sun_element(U.size(0), Nc=U.size(-1))
@@ -46,25 +62,29 @@ def _test_toy_action():
     batch_size = 2
     Nc = 2
     U = random_sun_element(batch_size, Nc=Nc)
-    gU = apply_gauge_transform(U)
+    gU = apply_conj(U)
 
     action = SUNToyAction(1.0)
     S = action(U)
     Sg = action(gU)
-    assert torch.allclose(S, Sg), \
-        '[FAILED: Action not gauge-invariant]'
+    assert torch.allclose(S, Sg), '[FAILED: Action not conjugation-invariant]'
     print('[PASSED]')
     
 
 if __name__ == '__main__': _test_toy_action()
 
 
-class SUNToyPolynomialAction:
-    """
-    Toy polynomial action over a single SU(N) matrix.
+class SUNToyPolynomialAction(Action):
+    r"""
+    Toy polynomial action over a single :math:`{\rm SU}(N)` matrix.
 
-    The polynomial action is defined as in Eqn. (18) of [2008.05456]
-    as a real trace over sums of powers of the matrix.
+    The action is defined as in Eqn. (18) of [`2008.05456`]:
+
+    .. math::
+
+        S(U) = -\frac{\beta}{N}{\rm Re}{\rm Tr}\left[\sum_{n} c_n U^n\right],
+
+    where :math:`U^n` denotes repeated matrix multiplication.
 
     Args:
         beta (float): Inverse coupling strength
@@ -79,22 +99,29 @@ class SUNToyPolynomialAction:
 
     @property
     def beta(self) -> float:
+        """Retrieves the coupling strength `beta`."""
         return self.__beta
 
-    def __call__(self, U):
+    def __call__(self, U: torch.Tensor) -> torch.Tensor:
+        """Computes the action as a function of group elements `U`."""
         Nc = U.shape[-1]
         action = 0
         for i, c in enumerate(self.coeffs):
             action += c * torch.matrix_power(U, i+1)
         return -(self.beta / Nc) * trace(action).real
 
-    def value_eigs(self, thetas):
-        """
-        Evaluates the action as a function of the eigenangles `thetas`
-        of the SU(N) matrix.
+    def value_eigs(self, thetas: torch.Tensor) -> torch.Tensor:
+        r"""
+        Evaluates the action in terms of the matrix eigenangles `thetas`.
+
+        In the angular representation, the action is given by
+
+        .. math::
+
+            S(\theta) = -\frac{\beta}{N} \sum_n c_n \sum_j\cos(n \theta_j)
 
         Args:
-            thetas: Batch of Nc eigenangles of the matrices
+            thetas (Tensor): Batch of eigenangles, shaped `[B, Nc]`
 
         Returns:
             Batch of action values
@@ -105,13 +132,18 @@ class SUNToyPolynomialAction:
             S += c * torch.cos((i+1) * thetas).sum(-1)
         return -(self.beta / Nc) * S
 
-    def force_eigs(self, thetas):
-        """
-        Evaluates the minus gradient of the action as a function
-        of the eigenangles `thetas` of the SU(N) matrix.
+    def force_eigs(self, thetas: torch.Tensor) -> torch.Tensor:
+        r"""
+        Evaluates the force as a function of the matrix eigenangles `thetas`.
+
+        In the angular representation, the force is given by
+
+        .. math::
+
+            F(\theta) = -\frac{\beta}{N} \sum_n n c_n \sin(n\theta)
 
         Args:
-            thetas: Batch of Nc eigenangles of the matrices
+            thetas (Tensor): Batch of eigenangles, shaped `[B, Nc]`
 
         Returns:
             Batch of force values
@@ -159,17 +191,26 @@ def _test_toy_polynomial_action():
 if __name__ == '__main__': _test_toy_polynomial_action()
 
 
-class SUNPrincipalChiralAction:
-    """
-    Action for the SU(N) x SU(N) Principle Chiral model
-    on the lattice.
+class SUNPrincipalChiralAction(Action):
+    r"""
+    Action for the :math:`{\rm SU}(N) \times {\rm SU}(N)` Principle Chiral
+    model on the lattice.
+
+    The action is defined as
+
+    .. math::
+
+        S[U] = -\beta N \sum_x \sum_\mu {\rm Re}{\rm Tr}\left[
+                U^\dagger(x) U(x + \hat{\mu})
+            \right]
 
     This theory enjoys two noteworthy symmetries:
-        - local charge conjugation invariance: U(x) -> U^dagger(x)
-        - global left-right multiplication invariance: U(x) -> V_L U(X) V_R^dagger
+        - local charge conjugation invariance: :math:`U(x) \to U^\dagger(x)`
+        - global left-right multiplication invariance: 
+            :math:`U(x) \to V_L U(X) V_R^\dagger`
 
     Args:
-        beta (float): Inverse temperature parameter, equal to 1 / (Nc * T)
+        beta (float): Inverse temperature parameter
     """
     def __init__(self, beta: float):
         self.__beta = beta
@@ -187,7 +228,7 @@ class SUNPrincipalChiralAction:
         for mu in range(Nd):
             action_density += trace(adjoint(U) @ roll(U, -1, mu+1)).real
 
-        dims = tuple(range(1, Nd + 1))
+        dims = tuple(range(1, Nd+1))
         return -self.beta * Nc * action_density.sum(dims)
 
 
@@ -205,14 +246,14 @@ def _test_pcm_action():
     # Charge conjugation symmetry
     S_adj = action(adjoint(U))
     assert torch.allclose(S, S_adj), \
-        '[FAILED: action should be invariant under charge conj parity]'
+        '[FAILED: action not invariant under local charge conjugation]'
 
     # Global SU(N)_L x SU(N)_R symmetry
     V_L = random_sun_lattice((1, 1, 1), Nc=2)
     V_R = random_sun_lattice((1, 1, 1), Nc=2)
     S_lr = action(V_L @ U @ adjoint(V_R))
     assert torch.allclose(S, S_lr), \
-        '[FAILED: action should be invariant under global left-right conjugation]'
+        '[FAILED: action not invariant under global left-right conjugation]'
     
     print('[PASSED]')
 
